7f62f09cf0b0bd48db1c1fbcbfedbd29a053733b
Misael Cabral <misaelrc.dev@gmail.com>
2025-04-17 18:11:26 -0300

    Criado as rotas de Register e Login

--- routes/api.php
+++ routes/api.php
@@ -1,8 +1,13 @@
 <?php
 
+use ApiMultipurpose\Http\Controllers\AuthController;
+use ApiMultipurpose\Http\Controllers\LoginController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/user', function (Request $request) {
-    return $request->user();
-})->middleware('auth:sanctum');
+Route::post('/register', [AuthController::class, 'store']);
+Route::post('/login', [LoginController::class, 'store']);
+
+// Route::get('/user', function (Request $request) {
+//     return $request->user();
+// })->middleware('auth:sanctum');
