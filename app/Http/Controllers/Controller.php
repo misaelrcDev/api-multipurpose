85f6bdcc01c5ebdd804faf4dd90dab6184d34344
Misael Cabral <misaelrc.dev@gmail.com>
2025-04-17 17:04:24 -0300

    first commit

---
+++ app/Http/Controllers/Controller.php
@@ -0,0 +1,8 @@
+<?php
+
+namespace ApiMultipurpose\Http\Controllers;
+
+abstract class Controller
+{
+    //
+}
---
+++ bootstrap/providers.php
@@ -0,0 +1,5 @@
+<?php
+
+return [
+    ApiMultipurpose\Providers\AppServiceProvider::class,
+];
