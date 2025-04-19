f0823d19962d5b2c8ffe5db5558525862918a270
Misael Cabral <misaelrc.dev@gmail.com>
2025-04-19 15:47:08 -0300

    Trocado a logica de login para dentro de Auth no controller

--- app/Http/Controllers/Auth.php
+++ app/Http/Controllers/Auth.php
@@ -7,7 +7,7 @@ use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Hash;
 use Illuminate\Validation\ValidationException;
 
-class LoginController extends Controller
+class Auth extends Controller
 {
 
     public function login(Request $request)
@@ -32,4 +32,28 @@ class LoginController extends Controller
             ]);
         }
     }
+
+    public function register(Request $request)
+    {
+        $validatedData = $request->validate([
+            'name' => 'required|string|max:255',
+            'email' => 'required|string|email|max:255|unique:users',
+            'password' => 'required|string',
+        ]);
+
+        $user = User::create([
+            'name' => $validatedData['name'],
+            'email' => $validatedData['email'],
+            'password' => Hash::make($validatedData['password']),
+        ]);
+
+        $token = $user->createToken('auth_token')->plainTextToken;
+
+        return response()->json([
+            'access_token' => $token,
+            'token_type' => 'Bearer',
+            'user' => $user
+        ]);
+    }
+
 }
--- routes/api.php
+++ routes/api.php
@@ -1,12 +1,20 @@
 <?php
 
-use ApiMultipurpose\Http\Controllers\AuthController;
+use ApiMultipurpose\Http\Controllers\Auth;
 use ApiMultipurpose\Http\Controllers\LoginController;
+use ApiMultipurpose\Http\Controllers\UserController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
-Route::post('/register', [AuthController::class, 'store']);
-Route::post('/login', [LoginController::class, 'store']);
+Route::post('/register', [Auth::class, 'register']);
+Route::post('/login', [Auth::class, 'login']);
+
+Route::group(['prefix' => 'users', 'middleware' => 'auth:sanctum'], fn () => [
+    Route::get('/', [UserController::class, 'index']),
+    Route::get('/{id}', [UserController::class, 'show']),
+    Route::put('/{id}', [UserController::class, 'update']),
+    Route::delete('/{id}', [UserController::class, 'destroy']),
+]);
 
 // Route::get('/user', function (Request $request) {
 //     return $request->user();
