4fa7f890ccc13d7ff04d0a2cd38709dd8dc8d18f
Misael Cabral <misaelrc.dev@gmail.com>
2025-04-30 10:29:09 -0300

    Criado as tabelas e o model de Conversation

---
+++ app/Models/Conversation.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace ApiMultipurpose\Models;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Conversation extends Model
+{
+    protected $fillable = [
+        'type',
+        'created_by',
+    ];
+
+    public function users()
+    {
+        return $this->belongsToMany(User::class);
+    }
+
+    public function messages()
+    {
+        return $this->hasMany(Message::class);
+    }
+}
