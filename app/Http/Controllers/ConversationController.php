ca6abbe9a10b931a84e0f7276d1eca46b7c8c50f
Misael Cabral <misaelrc.dev@gmail.com>
2025-04-30 14:41:24 -0300

    Implementado metodo de criar nova conversa

--- app/Http/Controllers/ConversationController.php
+++ app/Http/Controllers/ConversationController.php
@@ -2,6 +2,7 @@
 
 namespace ApiMultipurpose\Http\Controllers;
 
+use ApiMultipurpose\Models\Conversation;
 use Illuminate\Http\Request;
 
 class ConversationController extends Controller
@@ -16,5 +17,26 @@ class ConversationController extends Controller
             'message' => 'success',
             'data' => $conversations,
         ], 200);
+    }
+
+    public function store(Request $request)
+    {
+        $data = $request->validate([
+            'type' => 'required|string|in:private,group',
+            'participants' => 'required|array|min:1', // IDs dos participantes
+        ]);
+
+        $conversation = Conversation::create([
+            'type' => $data['type'],
+            'created_by' => auth()->id(),
+        ]);
+
+        // Relaciona os participantes à conversa
+        $conversation->users()->attach(array_merge($data['participants'], [auth()->id()]));
+
+        return response()->json([
+            'message' => 'Conversation created successfully',
+            'data' => $conversation,
+        ], 201);
     }
 }
